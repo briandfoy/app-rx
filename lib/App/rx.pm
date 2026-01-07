79ba852c79149b6cb5650ffe87ae924ed69569b4
brian d foy <briandfoy@pobox.com>
2026-01-07 13:21:34 -0500

    Normalize and update copyright

--- lib/App/rx.pm
+++ lib/App/rx.pm
@@ -345,7 +345,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2024, brian d foy, All Rights Reserved.
+Copyright © 2024-2026, brian d foy, All Rights Reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
